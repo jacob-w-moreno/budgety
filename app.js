14d3f5521d72d1acdcdb055a126ac5e38cec7e23
Mac <mac@MacBook-Air.hsd1.ut.comcast.net>
2020-06-19 00:07:03 -0600

    Structure is set up and a few of the controllers are working

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
 const budgetController = (function(){
-  // some code
+
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+  
+  const Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+  return {
+    addItem: function(type, des, val) {
+
+      var newItem;
+
+      // Creates a new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+      } else {
+        ID = 0
+      };
+
+      // Creates a new item based on the type.
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else 
+      if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into data where it's supposed to go.
+      data.allItems[type].push(newItem);
+
+      // Return the new element.
+      return newItem;
+
+    },
+    testing: function(){
+      console.log(data);
+    }
+  }
+
 })();
 
 
@@ -16,13 +71,17 @@ const userInterfaceController = (function(){
   }
 
   return {
+
     getInput: function(){
+
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will either be 'inc' or 'exp'
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       }
+
     },
+
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -36,25 +95,48 @@ const userInterfaceController = (function(){
 
 const controller = (function(budgetCtrl, UICtrl){
 
-  let DOM = UICtrl.getDOMstrings();
+  const setupEventListeners = function () {
+
+    const DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+      
+    });
+
+  }
 
   const ctrlAddItem = function () {
+
+    let input, newItem;
+
     // 1. Get the field input data.
-    var input = UICtrl.getInput();
-    console.log(input)
+    input = UICtrl.getInput();
+
     // 2. Add the item to the budget controller.
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to the UI.
+
     // 4. Calculate the budget.
+
     // 5. Display the budget.
+
   }
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
-  document.addEventListener('keypress', function(event){
+  return {
+    init: function(){
 
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+      setupEventListeners();
+
     }
+  }
 
-
-  })
 })(budgetController, userInterfaceController);
+
+controller.init();
